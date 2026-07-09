565f02cc3df5d837ccbde1f0add31326497076b5
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-10 00:19:47 +0100

    add rich, pyfiglet library for prettier display

--- ui/display.py
+++ ui/display.py
@@ -1,7 +1,22 @@
+from pyfiglet import figlet_format
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+def show_logo():
+    logo = figlet_format("GRAVITAS", font="ansi_shadow")
+    console.print(logo, style="white")
+
+
+
 def show_tasks(tasks):
-    print('='*10)
-    print('Tasks')
-    print('='*10)
+    table = Table(title="Tasks", show_header=True, header_style="bold green")
+
+    table.add_column('ID')
+    table.add_column('Task')
 
     for index, task in enumerate(tasks, start=1):
-        print(f'{index}. {task}')
+        table.add_row(str(index), task)
+
+    console.print(table)
