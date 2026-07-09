c71e87763bd919c3199697a8a23f3ebf70bf4a40
codewithraphael <raphaeldaniel223@gmail.com>
2026-07-09 23:30:52 +0100

    add: Persisient storage - making tasks survive after program closes

--- ui/menu.py
+++ ui/menu.py
@@ -1,6 +1,6 @@
 def show_menu():
     print('='*100)
-    print(f"GRAVITAS : /'gravItas/ noun: ability to remain calm, composed & respected under pressure")
+    print(f"GRAVITAS : /'gravItas/ noun: ability to remain calm")
     print('='*100)
     print('[1.] Add Task')
     print('[2.] View Task')
